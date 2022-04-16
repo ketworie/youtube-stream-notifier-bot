b6026c918eb5677df0914ebb7de5291ea4818652
kalinin-ng <KalininNG@intech.rshb.ru>
2022-04-16 23:00:29 +0400

    Added pk tags

--- db/model.go
+++ db/model.go
@@ -3,26 +3,26 @@ package db
 import "time"
 
 type Chat struct {
-	Id       int64
+	Id       int64 `bun:",pk"`
 	TimeZone *string
 	Enabled  bool
 }
 
 type Channel struct {
-	Id           string
+	Id           string `bun:",pk"`
 	Title        string
 	LeaseSeconds *int
 	LastUpdate   time.Time
 }
 
 type Subscription struct {
-	Id        int64
+	Id        int64 `bun:",pk,autoincrement"`
 	ChatId    int64
 	ChannelId string
 }
 
 type DoneStream struct {
-	Id           string
+	Id           string `bun:",pk"`
 	DoneUpcoming bool
 	DoneLive     bool
 }
